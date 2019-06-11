80eb66bf7288c2082754609d9eeff924a7976379
leseixas <seixasle@gmail.com>
2019-06-11 18:55:08 -0300

    Testing superdense.py with Aer qasm simulator and IBMQ device

--- superdense.py
+++ superdense.py
@@ -10,15 +10,15 @@ from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit import execute
 
 # QASM simulator
-from qiskit import Aer
-backend = Aer.get_backend('qasm_simulator')
+#from qiskit import Aer
+#backend = Aer.get_backend('qasm_simulator')
 
 # IBM Q
-#from qiskit import IBMQ
-#from qiskit.providers.ibmq import least_busy
-#IBMQ.load_accounts()
-#large_enough_devices = IBMQ.backends(filters=lambda x: x.configuration().n_qubits > 3 and not x.configuration().simulator)
-#backend = least_busy(large_enough_devices)
+from qiskit import IBMQ
+from qiskit.providers.ibmq import least_busy
+IBMQ.load_accounts()
+large_enough_devices = IBMQ.backends(filters=lambda x: x.configuration().n_qubits > 3 and not x.configuration().simulator)
+backend = least_busy(large_enough_devices)
 
 print("Backend:",backend)
 
@@ -47,5 +47,4 @@ job = execute(qc, backend, shots=1024)
 result = job.result()
 counts = result.get_counts(qc)
 print(counts)
-
 
