c6f18352b01053fb77f9be952c94f7fc3d4c7f63
Leandro Seixas Rocha <leandro.fisica@gmail.com>
2019-06-12 01:39:09 -0300

    Change 11 bit string with Pauli Y gate

--- superdense.py
+++ superdense.py
@@ -10,15 +10,15 @@ from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit import execute
 
 # QASM simulator
-#from qiskit import Aer
-#backend = Aer.get_backend('qasm_simulator')
+from qiskit import Aer
+backend = Aer.get_backend('qasm_simulator')
 
 # IBM Q
-from qiskit import IBMQ
-from qiskit.providers.ibmq import least_busy
-IBMQ.load_accounts()
-large_enough_devices = IBMQ.backends(filters=lambda x: x.configuration().n_qubits > 3 and not x.configuration().simulator)
-backend = least_busy(large_enough_devices)
+#from qiskit import IBMQ
+#from qiskit.providers.ibmq import least_busy
+#IBMQ.load_accounts()
+#large_enough_devices = IBMQ.backends(filters=lambda x: x.configuration().n_qubits > 3 and not x.configuration().simulator)
+#backend = least_busy(large_enough_devices)
 
 print("Backend:",backend)
 
@@ -33,8 +33,10 @@ qc.cx(0,1)
 # 01 = X
 # 11 = Z*X
 
-qc.z(0) # 01 or 11 
-#qc.x(0) # 10 or 11
+#qc.x(0) # 10
+#qc.y(0) # 11 # equal to X*Z
+#qc.z(0) # 01
+
 
 # Decoding
 
